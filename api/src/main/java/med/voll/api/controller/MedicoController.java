501e76c1b0163d276e2aa9b8f27d2e60232e0d5f
Ricardo Rodrigues Santana <140956804+Ric-ardo28@users.noreply.github.com>
2024-07-31 23:12:17 -0300

    java e spring config mysql

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,6 @@
 package med.voll.api.controller;
 
+import med.voll.api.medico.DadosCadastrosMedicos;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,6 +11,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
+    public void cadastrarMedico(@RequestBody DadosCadastrosMedicos dados){
+        System.out.println(dados);
 
     }
 }
